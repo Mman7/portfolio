9eca3a4a4170d05ea7139d7291af3606d87626c1
Eric Man <youzai0427@gmail.com>
2025-11-08 03:50:57 +0800

    added mobile avatar

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,35 +1,47 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Avatar from "../assets/avatar.png";
+import MobileAvatar from "../assets/mobile_avatar.png";
 import { faGithub, faSquareLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { Link } from "react-scroll";
 
 export default function HeroSection() {
   return (
     <>
-      <div className="bg-pattern z-100 flex h-[calc(100vh-10rem)] flex-col items-center justify-end">
-        <img
-          src={Avatar}
-          alt="avatar"
-          draggable={false}
-          onDragStart={(e) => e.preventDefault()}
-          className="z-10 h-[calc(100%)] brightness-80 select-none"
-        />
-        <div className="absolute z-100 py-16">
-          <button className="btn btn-lg mx-4 rounded border border-sky-500 bg-white/10 backdrop-blur-sm duration-200 hover:scale-105 hover:bg-white/20">
+      <img
+        src={MobileAvatar}
+        alt="avatar"
+        draggable={false}
+        onDragStart={(e) => e.preventDefault()}
+        className="z-10 mt-30 mb-20 h-72 brightness-80 select-none"
+      />
+      <img
+        src={Avatar}
+        alt="avatar"
+        draggable={false}
+        onDragStart={(e) => e.preventDefault()}
+        className="z-10 hidden h-[calc(100%)] brightness-75 select-none md:block"
+      />
+
+      <div className="absolute z-100 py-16">
+        <Link to="projects" duration={500} smooth={true} offset={-100}>
+          <button className="btn btn-md sm:btn-lg mx-4 rounded border border-sky-500 bg-white/10 backdrop-blur-sm duration-200 hover:scale-105 hover:bg-white/20">
             View Projects
           </button>
-          <button className="btn btn-lg mx-4 rounded border border-white bg-white/10 backdrop-blur-sm transition-all duration-200 hover:scale-105 hover:bg-white/20">
-            Get My Resume
+        </Link>
+        <Link to="contact" duration={500} smooth={true} offset={-100}>
+          <button className="btn btn-md sm:btn-lg mx-4 rounded border border-white bg-white/10 backdrop-blur-sm transition-all duration-200 hover:scale-105 hover:bg-white/20">
+            Contact me
           </button>
-        </div>
-        <SideContact />
+        </Link>
       </div>
-      {/* TODO custom font for background up and down spacing  */}
-      <div className="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-start text-[16rem] font-black tracking-tighter text-gray-600 opacity-55">
+      <SideContact />
+      {/* Background TEXT */}
+      <div className="absolute top-1/2 left-1/2 hidden w-full -translate-x-1/2 -translate-y-1/2 overflow-hidden md:block lg:p-30">
+        <h1 className="font-display text-start text-base/50 text-[16rem] font-black tracking-tighter text-gray-600 opacity-55">
           FRONTEND
         </h1>
-        <h1 className="text-end text-[16rem] font-black tracking-tighter text-gray-600 opacity-55">
+        <h1 className="font-display text-end text-base/60 text-[16rem] font-black tracking-tighter text-gray-600 opacity-55">
           DEVELOPER
         </h1>
       </div>
@@ -40,28 +52,30 @@ export default function HeroSection() {
 export function SideContact({ className }: { className?: string }) {
   return (
     <div
-      className={`absolute top-1/3 right-0 z-200 flex flex-col justify-between gap-10 rounded-xl bg-black/40 p-3 ${className}`}
+      className={`absolute top-1/8 right-0 z-200 hidden flex-col justify-between gap-10 rounded-xl bg-black/40 p-3 *:duration-300 md:top-1/3 md:flex ${className}`}
     >
       <a
-        href="http://"
+        href="https://www.linkedin.com/in/man-eric-2aa13b159/"
         target="_blank"
-        rel="noopener noreferrer"
         className="hover:text-white"
+        title="https://www.linkedin.com/in/man-eric-2aa13b159/"
       >
         <FontAwesomeIcon size="2xl" icon={faSquareLinkedin} />
       </a>
       <a
-        href="http://"
+        href="https://github.com/Mman7"
         target="_blank"
-        rel="noopener noreferrer"
+        title="https://github.com/Mman7"
         className="hover:text-white"
       >
         <FontAwesomeIcon size="2xl" icon={faGithub} />
       </a>
+      {/* TODO show user that copy */}
       <a
-        href="http://"
-        target="_blank"
-        rel="noopener noreferrer"
+        onClick={() => navigator.clipboard.writeText("youzai0427@gmail.com")}
+        id="13"
+        title="youzai0427@gmail.com"
+        href="mailto:youzai0427@gmail.com"
         className="hover:text-white"
       >
         <FontAwesomeIcon size="2xl" icon={faEnvelope} />
