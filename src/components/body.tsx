5f3912a2c212d33163ea607da9c63d9e97edbd69
Eric Man <youzai0427@gmail.com>
2025-11-08 03:50:31 +0800

    added scrolling element

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -3,15 +3,29 @@ import Contact from "./contact";
 import HeroSection from "./hero";
 import Projects from "./projects";
 import Skill from "./skill";
+import { Element } from "react-scroll";
 
 export default function Body() {
   return (
     <div className="bg-customBase h-dvh w-full">
-      <HeroSection />
-      <About />
-      <Projects />
-      <Skill />
-      <Contact />
+      <Element
+        name="hero"
+        className="bg-pattern overflow: auto z-100 flex h-1/2 scroll-mt-24 flex-col items-center justify-end md:h-[calc(100vh-10rem)]"
+      >
+        <HeroSection />
+      </Element>
+      <Element name="about">
+        <About />
+      </Element>
+      <Element name="projects">
+        <Projects />
+      </Element>
+      <Element name="skills">
+        <Skill />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
     </div>
   );
 }
